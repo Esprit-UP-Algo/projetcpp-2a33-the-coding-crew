681f9bc2a91b50465a7242b4e1ecc99b3942d40b
marammelki123 <maram.melki@esprit.tn>
2023-11-16 22:24:55 +0100

    Third commit main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,31 @@
-#include <iostream>
-#include <string>
 
-using namespace std ;
-int main ()
+#include "mainwindow.h"
+#include <QApplication>
+#include <QMessageBox>
+#include "connect.h"
+#include "conteneur.h"
+
+#include<QDebug>
+int main(int argc, char *argv[])
 {
-    cout <<"Hello world !¨!!!!"<<endl ;
-    cout <<"Hello world !¨!!!!"<<endl ;
-    return 0 ;
+    QApplication a(argc, argv);
+    Connection c;
+    bool test=c.createconnect();
+    MainWindow w;
 
+         if(test)
+        {w.show();
+        QMessageBox::critical(nullptr, QObject::tr("database is   open"),
+                    QObject::tr("connection successful.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+}
+    else
+        QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                    QObject::tr("connection failed.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+
+    return a.exec();
 }
